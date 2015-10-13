0df97de24e810bb4507bcd27a7e4bf6cd2c48460
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-13 19:57:33 +0000

    Creating branch for BioC 3.2 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_2/madman/Rpacks/cn.farms@109591 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/normalizeAverage.R
@@ -0,0 +1,44 @@
+#' Scales the range of the non-polymorphic data to the range of a given array.
+#' @param x Data matrix
+#' @param baselineArray Choose the baseline channel array.
+#' @param avg The function for averaging.
+#' @param targetAvg Value to which the array should be averaged.
+#' @param ... Further optional parameters.
+#' @return Normalized non-polymorphic data.
+#' @author Djork-Arne Clevert \email{okko@@clevert.de} and 
+#' Andreas Mitterecker \email{mitterecker@@bioinf.jku.at}
+#' @export 
+#' @examples
+#' x <- matrix(rnorm(100, 11), 20, 5)
+#' normalizeAverage(x, x[, 1])
+normalizeAverage <- function(x, baselineArray, avg = median, 
+        targetAvg = 2200, ...) {
+    
+    if (missing(baselineArray)) {
+        baselineArray <- x[, 1:2, drop = FALSE]
+    }
+    
+    ## Estimate the scale for each channel
+    scale <- apply(x, MARGIN = 2, FUN = avg, ...)
+    
+    ##FIXME: 
+    #scale1 <- apply(x, MARGIN = 2, FUN = avg, ...)
+    scale1 <- median(baselineArray)
+    targetAvg <- 2200
+    
+    ## Standardize so that the 'baseline' channel has scale one.
+    scale <- scale / scale1
+    
+    ## Rescale to target averages?
+    if (!is.null(targetAvg)) {
+        rho <- (scale1 / targetAvg)
+        scale <- rho * scale
+    }
+    
+    ## Rescale so that all channels have the same scale
+    for (cc in 1:ncol(x)) {
+        x[, cc] <- x[, cc] / scale[cc]
+    }
+    
+    return(x)
+}
---
+++ R/normalizeQuantiles.R
@@ -0,0 +1,161 @@
+#' Normalization Quantiles
+#' @param filenames filenames
+#' @param cores cores
+#' @param batch batch
+#' @param annotDir annotDir
+#' @param runtype Mode how the results are saved. Possible values are ff or bm. 
+#' If ff is chosen the data will not be saved automatically. 
+#' With bm the results will be saved permanently. 
+#' @param pkgname Optional parameter for the CEL mapping.
+#' @param saveFile Name of the file to save.
+#' @importFrom preprocessCore normalize.quantiles.use.target
+#' @return The normalized data.
+#' @author Djork-Arne Clevert \email{okko@@clevert.de} and 
+#' Andreas Mitterecker \email{mitterecker@@bioinf.jku.at}
+normalizeQuantiles <- function (
+        filenames, 
+        cores = 1, 
+        batch = NULL, 
+        annotDir = NULL, 
+        runtype = "ff", 
+        pkgname = NULL, 
+        saveFile = "normDataQuant") { 
+    
+    ## assure correct file extension
+    saveFile <- gsub("\\.RData", "", saveFile)
+    saveFile <- gsub("\\.rda", "", saveFile)
+    saveFile <- paste(saveFile, ".RData", sep = "")
+    
+    if(is.null(batch)) {
+        batch <- rep(1, length(filenames))
+    } 
+    
+    if (length(filenames) != length(batch)) {
+        stop("Filenames and batch need to have the same dimension")
+    }
+    
+    if(is.null(pkgname)) {
+        mapping <- affxparser::readCelHeader(filenames[1])$chiptype
+        pkgname <- oligo::cleanPlatformName(mapping)
+    }
+    
+    if (is.null(annotDir)) {
+        vers <- dir(file.path("annotation", pkgname))[1]
+        annotDir <- normalizePath(file.path("annotation", pkgname, vers))
+    }
+    
+    cat(paste(Sys.time(), "|   Annotation directory: ", 
+                    annotDir, " \n"))
+    
+    normFile <- file.path(annotDir, "annotNormalization.RData")
+    
+    if(file.exists(normFile)) {
+        pmfeature <- data.frame()
+        idxOfAlleleA <- vector()
+        idxOfAlleleB <- vector()
+        load(normFile)    
+    } else {
+        stop("Something went wrong with the annotation")
+    }    
+    
+    nbrOfSamples <- length(filenames)
+    nbrOfProbes <- length(which(pmfeature[, "allele"] == 1))
+    
+    intensity <- createMatrix(runtype, nbrOfProbes, nbrOfSamples, 
+            type = "double", bmName = gsub("\\.RData", "", saveFile))
+    
+       
+    if (cores == 1) {
+        tmpExprs <- affxparser::readCelIntensities(filenames, 
+                indices = pmfeature$fid)
+        tmpExprs <- (tmpExprs[idxOfAlleleA, ] + tmpExprs[idxOfAlleleB, ]) / 2
+        tmpExprs <- log2(tmpExprs)
+        x <- preprocessCore::normalize.quantiles(tmpExprs)
+        intensity[] <- x[]
+        rm(x, tmpExprs)
+    } else {
+        
+        ## determine the best array 
+        baseline <- determineBaselineArray(
+                filenames, nbrOfProbes = 10000, runtype = "ff", pmfeature$fid, 
+                cores = cores)
+        target <- as.vector(
+                affxparser::readCelIntensities(filenames[baseline], 
+                        indices = pmfeature$fid))
+        target <- target[idxOfAlleleA] + target[idxOfAlleleB]
+        
+        fidTmp <- pmfeature$fid
+        
+        gc()
+        sfInit(parallel = TRUE, cpus = cores, type = "SOCK")   
+        cnLibrary("cn.farms", character.only = TRUE, verbose = FALSE)
+        cnLibrary("affxparser", character.only = TRUE, verbose = FALSE)
+        cnLibrary("oligo", character.only = TRUE, verbose = FALSE)
+        cnLibrary("preprocessCore", character.only = TRUE, verbose = FALSE)
+        exportList <- c("fidTmp", "target", "idxOfAlleleA", 
+                "idxOfAlleleB", "intensity")
+        suppressWarnings(sfExport(list = exportList))
+        
+        cat(paste(Sys.time(), "|   Starting normalization \n"))
+        res <- suppressWarnings(sfLapply(1:nbrOfSamples, normalizeQuantilesH01, 
+                        filenames))
+    }
+    
+    sfStop()    
+    cat(paste(Sys.time(), "|   Normalization done \n"))
+    
+    
+    eSet <- new("ExpressionSet")
+    
+    ## assay data    
+    assayData(eSet) <- list(intensity = intensity)
+    
+    ## pheno data
+    phenoData(eSet) <- new("AnnotatedDataFrame", data = data.frame(filenames,
+                    batch = batch))
+    
+    ## feature data
+    featureData(eSet) <- new("AnnotatedDataFrame", 
+            data = pmfeature[pmfeature$allele == 1, c("fid", "fsetid")])
+    
+    ## experiment data
+    experimentData(eSet) <- new("MIAME", 
+            other = list(
+                    annotDir = annotDir, 
+                    normalization = "quantiles", 
+                    type = "normData"))    
+    
+    ## annotation
+    annotation(eSet) <- pkgname
+    
+    ## sample names
+    sampleNames(eSet) <- gsub("\\.[Cc][Ee][Ll]", "", basename(filenames))    
+    
+    return(eSet)    
+}
+
+#' Helper function
+#' @param ii ii
+#' @param filenames filenames
+#' @return Some data
+#' @author Djork-Arne Clevert \email{okko@@clevert.de} and 
+#' Andreas Mitterecker \email{mitterecker@@bioinf.jku.at}
+#' @noRd
+normalizeQuantilesH01 <- function (ii, filenames) {
+    
+    ## non-visible bindings
+    fidTmp <- fidTmp
+    idxOfAlleleA <- idxOfAlleleA
+    idxOfAlleleB <- idxOfAlleleB
+    target <- target
+    
+    
+    tmpExprs <- affxparser::readCelIntensities(filenames[ii], indices = fidTmp)
+    tmpExprs <- (tmpExprs[idxOfAlleleA] + tmpExprs[idxOfAlleleB]) / 2
+    intensity[, ii] <- log2(preprocessCore::normalize.quantiles.use.target(
+                    as.matrix(tmpExprs), target))
+    #intensity[, ii] <- log2(target[rank(tmpExprs)])
+}
+
+
+
